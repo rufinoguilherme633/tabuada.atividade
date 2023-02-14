3523dbe37f24cf466bcb11a316044160c8f2fb9c
GUILHERME RUFINO CAMPOS <22102603@CFP127ALUNO.LOCAL>
2023-02-14 16:46:34 -0300

    tratamento de alguns erros e quSE TERMINO DA LOGICA DE CALCULO

--- app.js
+++ app.js
@@ -12,32 +12,80 @@ var entradaDados = readline.createInterface({
 
 
 
-entradaDados.question('digite número inicial \n', function(numeroInicial) {
+entradaDados.question('digite tabuaada inicial \n', function(numeroInicial) {
     let primeroMutiplicando = numeroInicial;
     //  if (primeroMutiplicando == '') {
     // console.log("Erro : é preciso responder essa pergunta ")
     // }
-    entradaDados.question('Digite o número final \n', function(numeroFinal) {
+
+    if (isNaN(primeroMutiplicando)) {
+        console.log("É necessario que este campo seja um número")
+        process.exit();
+    }else if (primeroMutiplicando == 0) {
+        console.log("o número 0 não é permitdo")
+        process.exit();
+    }
+    entradaDados.question('Digite o tabuada final \n', function(numeroFinal) {
         let ultimoMultiplicando = numeroFinal;
         //  if (ultimoMultiplicando == '') {
         // console.log("Erro : é preciso responder essa pergunta ")
         // }
+        if (isNaN(ultimoMultiplicando)) {
+            console.log("É necessario que este campo seja um número")
+            process.exit();
+        }
 
         if (primeroMutiplicando == '' || ultimoMultiplicando == '') {
             console.log("Erro : é preciso preencher todos os campos ")
-        } else if (primeroMutiplicando > 200 || ultimoMultiplicando < 2 || ultimoMultiplicando > 200) {
-            console.log("O sistema só aceita números entre 0 e 200")
+            process.exit();
+        } else if ( primeroMutiplicando < 2||primeroMutiplicando > 200 || ultimoMultiplicando < 2 || ultimoMultiplicando > 200) {
+            console.log("O sistema só aceita números entre 2 e 200")
+            process.exit();
         }
 
         // console.log("*****************************************")
         // console.log("O sistema irá calcular do numero 1 até o 50")
         // console.log("*****************************************")
 
-        let calcular = matematica.multiplicar(primeroMutiplicando, ultimoMultiplicando);
+       entradaDados.question('Digite o começo da tabuada\n' ,function(inicioTabuada) {
+           let tabuadaInicio = inicioTabuada;
 
+           if (tabuadaInicio < 1 || tabuadaInicio > 50) {
+            console.log("O sistema so aceita o numeros entre 1 e 50")
+            process.exit();
+        }
 
+        
+           entradaDados.question('Digite o fim da tabuada\n' ,function(fimTabuada) {
+            let tabuadafim = fimTabuada;
+            if (isNaN(tabuadafim)) {
+                console.log("É necessario que este campo seja um número")
+                process.exit();
+            }
 
+            if (tabuadaInicio == '' || tabuadafim == '') {
+                console.log("Erro : é preciso preencher todos os campos ")
+            process.exit();
+            }   else if (isNaN(inicioTabuada)) {
+                console.log("É necessario que este campo seja um número")
+                process.exit();
+            }  else {
+                if (tabuadafim < 1 || tabuadafim > 50) {
+                    console.log("O sistema so aceita o numeros entre 1 e 50")
+                    process.exit();
+                } else {
+                    let calcular = matematica.multiplicar(primeroMutiplicando, ultimoMultiplicando, tabuadaInicio,tabuadafim);
+                    process.exit();
+                   
+                }
+            }
 
+           
+            
+             
+        })
+
+       })
 
     })
 })
--- modulo/tabuada.js
+++ modulo/tabuada.js
@@ -1,30 +1,38 @@
-let multiplicar = function(primeroMultiplicando, ultimoMultiplicando) {
-    let valorPrimeiro = Number(primeroMultiplicando);
-    let valorUltimo = Number(ultimoMultiplicando);
-    let resultado;
-    let cont = 0;
-    let contPr = 0;
+//let multiplicar = function(primeroMultiplicando, ultimoMultiplicando) {
+    let multiplicar = function(primeroMultiplicando, ultimoMultiplicando, inicioMultiplicando,fimMultiplicando) {  
+        let valorPrimeiro = Number(primeroMultiplicando);
+        let valorUltimo = Number(ultimoMultiplicando);
+        let resultado;
+        let cont;
+        let contPr = 0;
+        let valor1 = Number(inicioMultiplicando)  ;
+        let valor2 = Number(fimMultiplicando)  ;
+    
+        for (contPr = valorPrimeiro; contPr <= valorUltimo; contPr++) {
+            // resultado = contPr
+            // console.log(resultado)
+            // for (let cont = 1; cont <= 3; cont++) {
+            for (let cont =  valor1; cont <= valor2; cont++) {
+                resultado = contPr * cont
 
-
-
-
-    for (contPr = valorPrimeiro; contPr <= valorUltimo; contPr++) {
-        // resultado = contPr
-        // console.log(resultado)
-
-        for (let cont = 1; cont <= 3; cont++) {
-            resultado = contPr * cont
-            console.log(contPr + " x " + cont + " = " + resultado)
+               // console.log("TABUADA do " + contPr + "\n")
+                
+                console.log(contPr + " x " + cont + " = " + resultado)
+                
+                
+            }
+            
+            console.log("----------------------------------------")
+            
         }
-        console.log("----------------------------------------")
+        
+    
+    
+    
     }
-
-
-
-}
-
-// multiplicar(1, 4)
-
-module.exports = {
-    multiplicar
-}
+    
+     //multiplicar(1, 4,1,10)
+    
+    module.exports = {
+        multiplicar
+    }
